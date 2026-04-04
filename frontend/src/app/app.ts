0771877c3f724fe1fa695c9be598f5a39b7674a4
Sky-Miram <siddharth.ky@kalvium.community>
2026-04-04 15:08:53 +0530

    feat: implement route protection and guards with logout functionality for issue #22

--- frontend/src/app/app.ts
+++ frontend/src/app/app.ts
@@ -1,13 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('frontend');
+
+  constructor(public authService: AuthService, private router: Router) {}
+
+  logout() {
+    this.authService.logout();
+    this.router.navigate(['/login']);
+  }
 }
